bcb46fc858c8bd43106ecec6f374d5fa119dae84
hippo159 <46355784+hippo159@users.noreply.github.com>
2022-10-25 23:28:44 +0200

    feat: add random chuck joke

--- LABO3/weatherapp/src/Jokes.js
+++ LABO3/weatherapp/src/Jokes.js
@@ -1,3 +1,14 @@
+import Weather from '../src/Weather.js';
 export default class Jokes {
-
+    constructor() {
+        this.getJoke();
+    }
+    getJoke() {
+        fetch('https://api.chucknorris.io/jokes/random')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            document.querySelector(".joke__text").innerText = data.value;
+        });
+    }
 }
