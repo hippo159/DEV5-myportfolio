7ea1547898b759fce4127e485e76e5ce1d88c804
hippo159 <46355784+hippo159@users.noreply.github.com>
2022-10-25 23:20:09 +0200

    feat: add Jokes class

--- LABO3/weatherapp/main.js
+++ LABO3/weatherapp/main.js
@@ -1,3 +1,5 @@
 import './style.css';
 import Weather from './src/Weather.js';
+import Jokes from './src/Jokes.js';
 const weather = new Weather("70b9db07fa264169866201652222510");
+const joke = new Jokes();
