a25684ca993bfb83fd092ab5f5ef55cc865afdd2
hippo159 <46355784+hippo159@users.noreply.github.com>
2022-10-26 00:08:40 +0200

    fix: base joke off of weather status

--- LABO3/weatherapp/main.js
+++ LABO3/weatherapp/main.js
@@ -2,6 +2,6 @@ import './style.css';
 import Weather from './src/Weather.js';
 import Jokes from './src/Jokes.js';
 const weather = new Weather("70b9db07fa264169866201652222510");
-const status = 'guns'; //document.querySelector(".weather__summary").innerText;
+const status = document.querySelector(".weather__summary").innerText;
 const joke = new Jokes(status);
 console.log(status)
