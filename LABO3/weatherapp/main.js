8ded928fa159ffb0686b45a171ed08a73136e91d
hippo159 <46355784+hippo159@users.noreply.github.com>
2022-12-06 12:39:02 +0100

    feat: update api key

--- LABO3/weatherapp/main.js
+++ LABO3/weatherapp/main.js
@@ -1,7 +1,7 @@
 import './style.css';
 import Weather from './src/Weather.js';
 import Jokes from './src/Jokes.js';
-const weather = new Weather("70b9db07fa264169866201652222510");
+const weather = new Weather("5a9a2c45322348aeb6a113745220612");
 const status = document.querySelector(".weather__summary").innerText;
 const joke = new Jokes(status);
 console.log(status)
